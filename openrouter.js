a5fc3f3b9e6e8ce9e83af0415f2c21f084a8708e
Samar-dot188 <gosaliasamar@gmail.com>
2026-02-01 18:19:50 +0530

    Update openrouter.js

--- openrouter.js
+++ openrouter.js
@@ -6,10 +6,10 @@ export async function handler(event, context) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.OPENROUTER_KEY}`
+        "Authorization": `Bearer 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`
       },
       body: JSON.stringify({
-        model: "gpt-4o-mini",
+        model: "google/gemma-3-27b-it:free",
         messages: [{ role: "user", content: prompt }]
       })
     });
