fb92d89ede49a3bc79595733fe955743bfe6762a
Samar-dot188 <gosaliasamar@gmail.com>
2026-02-02 17:54:56 +0530

    Update openrouter.js

--- functions/openrouter.js
+++ functions/openrouter.js
@@ -17,7 +17,7 @@ exports.handler = async (event) => {
                 "HTTP-Referer": "https://ecoverse.netlify.app", // Optional but good practice
             },
             body: JSON.stringify({
-                model: "google/gemma-3-27b-it:free",
+                model: "google/gemma-3-27b-it",
                 messages: [{ role: "system", content: "You are an environmental judge. Respond ONLY with JSON." },
                                 {role: "user", content: `Evaluate this city: ${objects}. Format: {"rating": 5, "feedback": "...", "tip": "..."}` }
     ]
